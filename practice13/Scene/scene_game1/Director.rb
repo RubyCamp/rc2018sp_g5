04c17e747eb29996c1d3c9e738911fd43e8fbd8c
shocci1214 <shocci1214@gmail.com>
2018-03-06 22:48:40 +0900

    パワーゲージを追加,ショットを撃つとゲージ減少

--- practice13/Scene/scene_game1/Director.rb
+++ practice13/Scene/scene_game1/Director.rb
@@ -2,25 +2,27 @@ require_relative '../Lib/Player'
 require_relative '../Lib/Enemy'
 require_relative '../Lib/Tile'
 require_relative '../Lib/Haikei'#背景クラスのファイルを読み込み
+require_relative '../Lib/Info/PowerGage'
 
 module Game
   class Director
     def initialize
+      @powergage = PowerGage.new #パワーゲージ
       @haikei = Haikei.new#背景インスタンス
-      @tile = Tile.new
-      @player = Player.new
+      @tile = Tile.new #床
+      @player = Player.new #プレイヤー
       @playershots = [] #プレイヤーの弾
       @enemies = [] #敵配列
-      @count = 0#敵キャラ生成用
+      @count = 0#敵キャラ生成用カウント
     end
 
     def play
       @haikei.draw#背景を描画
       @tile.update #床を描画
-      Sprite.check(@tile,@player)
-      @player.update(@playershots)
-      Sprite.update([@playershots,@enemies])
-      Sprite.check(@playershots,@enemies)
+      Sprite.check(@tile,@player) #床とプレイヤーのめり込みチェック
+      @player.update(@playershots)#プレイヤーの弾の描画
+      Sprite.update([@playershots,@enemies])#プレイヤーの弾配列と敵配列の描画
+      Sprite.check(@playershots,@enemies)#プレイヤーの弾と敵配列の当たり判定
 
       if @count % 50 == 0
                 @enemies << Enemy.new(600,352)
@@ -28,6 +30,8 @@ module Game
 
         @count += 1
         Sprite.clean([@playershots,@enemies])
+
+        @powergage.update
 
     end
   end
