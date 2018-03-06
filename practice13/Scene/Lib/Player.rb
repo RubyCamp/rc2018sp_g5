57e878ecefaed55a232a7ef236a96b164f7ff5af
shocci1214 <shocci1214@gmail.com>
2018-03-06 23:00:21 +0900

    Playerクラスを整理

--- practice13/Scene/Lib/Player.rb
+++ practice13/Scene/Lib/Player.rb
@@ -15,46 +15,12 @@ class Player < Sprite
 
         #重力の設定
         y_move = (self.y - @y_prev) + @gravity
-        #すり抜け防止
-        if y_move > 31
-            y_move = 31
-        end
-        if y_move < -31
-            y_move = -31
-        end
         @y_prev = self.y
         self.y += y_move
         @gravity = 2
 
-        #床衝突判定
-        #キャラの右下端と左下端の座標が障害物パーツであった場合
-        # if collision_tile(self.x,self.y+31,@map) == 1 or collision_tile(self.x+31,self.y+31,@map) == 1
-        #     self.y = self.y/32*32 #32で割って余りを切り捨て、再び掛けたとき、Yが32の倍数になるようにする
-        #     @jump_ok = true #地面に接地しているのでジャンプを許可する
-        # else
-        #     @jump_ok = false #地面に接地していないので、ジャンプは許可しない
-        # end
-
         #左右移動
         self.x += Input.x * @speed
-
-
-        # #左右移動
-        # @selfprev = self.x
-        # if @kabe
-        #     self.x += Input.x * 10
-        # else
-        #     self.x == @selfprev
-        # end
-
-        # #右側壁衝突判定
-        # if collision_tile(self.x+31,self.y,@map) ==1 or collision_tile(self.x+31,self.y+31,@map) == 1
-        #     self.x = self.x/32*32
-        #     @kabe = false
-        # else
-        #     @kabe = true
-        # end
-
 
         #ジャンプ
         if Input.key_push?(K_SPACE) && @flg == 1
