95e45d54c492bdf0c4c4a60441e7d747dbe7398c
shocci1214 <shocci1214@gmail.com>
2018-03-06 16:02:28 +0900

    pracitice11を追加

--- practice11/Scene/Lib/Player.rb
+++ practice11/Scene/Lib/Player.rb
@@ -8,6 +8,7 @@ class Player < Sprite
         @gravity = 2
         @jump_ok = false
         @map = map
+        @selfprev = x
     end
 
     #対応する配置パーツ番号を返す
@@ -15,7 +16,7 @@ class Player < Sprite
         return array[y/32][x/32]
     end
 
-    def update
+    def update(playershot)
 
         #重力の設定
         y_move = (self.y - @y_prev) + @gravity
@@ -32,18 +33,29 @@ class Player < Sprite
 
         #床衝突判定
         #キャラの右下端と左下端の座標が障害物パーツであった場合
-        if  collision_tile(self.x+31,self.y+31,@map) == 1
+        if collision_tile(self.x,self.y+31,@map) == 1 or collision_tile(self.x+31,self.y+31,@map) == 1
             self.y = self.y/32*32 #32で割って余りを切り捨て、再び掛けたとき、Yが32の倍数になるようにする
             @jump_ok = true #地面に接地しているのでジャンプを許可する
         else
             @jump_ok = false #地面に接地していないので、ジャンプは許可しない
         end
 
-        #左側衝突判定
-        if collision_tile(self.x,self.y+31,@map)
 
         #左右移動
-        self.x += Input.x * 10
+        @selfprev = self.x
+        if @kabe
+            self.x += Input.x * 10
+        else
+            self.x == @selfprev
+        end
+
+        #右側壁衝突判定
+        if collision_tile(self.x+31,self.y,@map) ==1 or collision_tile(self.x+31,self.y+31,@map) == 1
+            self.x = self.x/32*32
+            @kabe = false
+        else
+            @kabe = true
+        end
 
 
         #ジャンプ
@@ -52,7 +64,32 @@ class Player < Sprite
             @gravity = -20
         end
 
+        #弾
+        if Input.key_push?(K_B)
+          playershot << Playershot.new(self.x,self.y)
+        end
+
         #描画
         self.draw
     end
+end
+
+class Playershot < Sprite
+  def initialize(x,y)
+    super
+    self.x = x
+    self.y = y
+    self.image = Image.new(32,32,C_RED)
+  end
+  def update
+    self.draw
+    self.x += 10
+
+    if self.x > 640
+      self.vanish
+    end
+  end
+  def shot
+     self.vanish
+  end
 end
---
+++ practice11/Scene/scene_game1/Director.rb
@@ -0,0 +1,18 @@
+require_relative '../Lib/Player'
+require_relative '../Lib/Map'
+
+module Game
+    class Director
+        def initialize
+            @map = Map.new
+            @player = Player.new(200,200,@map.map)
+            @playershots = []
+        end
+
+        def play
+            @player.update(@playershots)
+            Sprite.update([@playershots])
+            Window.draw_tile(0,0,@map.map,@map.map_tile,0,0,@map.map[0].length,@map.map.length,-1)
+        end
+    end
+end
