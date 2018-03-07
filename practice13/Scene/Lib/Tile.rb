3c0e7f14fa3b2c957c79436d641f1fb980cdb0e2
shocci1214 <35397506+shocci1214@users.noreply.github.com>
2018-03-08 08:26:39 +0900

    Update Tile.rb

--- practice13/Scene/Lib/Tile.rb
+++ practice13/Scene/Lib/Tile.rb
@@ -1,6 +1,6 @@
 class Tile < Sprite
     def initialize
-      self.image = Image.new(1000,200,C_RED)
+      self.image = Image.new(1000,200,[160,82,45])
       self.x = 0
       self.y = 400
       @speed = 5
