b2473ef9ced762651ebbe8b6bae830e33c112e43
k5440215 <37126855+k5440215@users.noreply.github.com>
2018-03-08 01:50:09 +0000

    Update Enemy.rb
    床と空に当たったら上下に移動するようにしました

--- practice13/Scene/Lib/Enemy.rb
+++ practice13/Scene/Lib/Enemy.rb
@@ -4,11 +4,13 @@ class Enemy < Sprite
         self.x = x
         self.y = y
         self.image = Image.load('Scene/images/enemy.png')
+        @y_move = 5
     end
 
     def update
         self.draw
         self.x -= 10
+        self.y += @y_move
 
         if self.x < 0
             self.vanish
@@ -22,4 +24,11 @@ class Enemy < Sprite
     def shot
         self.vanish      
     end
+    def shot_tile
+        @y_move -= rand(5) + 1
+    end
+    def shot_sky
+        @y_move = rand(5) + 1
+    end
 end
+
