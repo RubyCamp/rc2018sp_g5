183b5aa76c594c87848bdc05f19db94ebacb3ea7
syamoji56 <36583417+syamoji56@users.noreply.github.com>
2018-03-08 11:23:17 +0900

    Update Director.rb

--- practice13/Scene/scene_gameover/Director.rb
+++ practice13/Scene/scene_gameover/Director.rb
@@ -3,11 +3,13 @@ module Gameover
         def initialize
             #Font
             @font_title = Font.new(30,'MSゴシック')
+            @gameover = Image.load('Scene/images/gameover.png')#タイトルの画像
+
         end
 
         def play
-            Window.draw_font(200,100,'ゲームオーバー！',@font_title)
-            Window.draw_font(200,200,'エスケープキーで終わる',@font_title)
+          Window.draw(90,100,@gameover)#タイトルを表示
+          Window.draw_font(200,350,'ボタンを押して終了',@font_title,:color=>[0,0,0])
 
         end
     end
